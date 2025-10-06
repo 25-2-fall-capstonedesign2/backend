cff5bf31fdc7638116ce8f3d2ac674350731bd58
KyuHyung Lee <kyuhunglee01@gmail.com>
2025-10-06 15:19:08 +0900

    Fix: Websocket related files deleted due to too many errors

--- src/main/java/com/capstone/backend/BackendApplication.java
+++ src/main/java/com/capstone/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package com.capstone.backend;
 
+import com.capstone.backend.config.WebSocketConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebSocketConfig.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
