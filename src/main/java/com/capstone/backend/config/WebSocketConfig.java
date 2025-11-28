0ad5932c9de12c47c670eb59e12cb4b002f4c13d
kyuhyunglee <kyuhunglee01@gmail.com>
2025-11-28 17:14:43 +0900

    chore: call message buffer size increased to 5MB

--- src/main/java/com/capstone/backend/config/WebSocketConfig.java
+++ src/main/java/com/capstone/backend/config/WebSocketConfig.java
@@ -3,10 +3,12 @@ package com.capstone.backend.config;
 import com.capstone.backend.handler.ClientWebSocketHandler;
 import com.capstone.backend.handler.GpuWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket // 순수 WebSocket 활성화
@@ -25,5 +27,14 @@ public class WebSocketConfig implements WebSocketConfigurer {
         // 2. GPU 워커 전용 엔드포인트
         registry.addHandler(gpuWebSocketHandler, "/ws-gpu")
                 .setAllowedOriginPatterns("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        // 텍스트와 바이너리 메시지 버퍼 크기를 1MB로 설정 (기본값 8KB)
+        container.setMaxTextMessageBufferSize(5 * 1024 * 1024);
+        container.setMaxBinaryMessageBufferSize(5 * 1024 * 1024);
+        return container;
     }
 }
